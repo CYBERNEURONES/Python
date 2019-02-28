d6f3ad7a469339190a9ce49e690dd3b5c4dd281b
ARIAS Frederic <frederic.arias@gmail.com>
2019-02-28 19:45:51 +0100

    Update JoplinCheckRessources.py

--- JoplinCheckRessources.py
+++ JoplinCheckRessources.py
@@ -1,5 +1,5 @@
 #
-# Version 1 
+# Version 2 
 # for Python 3
 # 
 #   ARIAS Frederic
@@ -20,7 +20,7 @@ start = time.time()
 #Token
 ip = "127.0.0.1"
 port = "41184"
-token = "Put the token here"
+token = "Put token here"
 nb_request = 0
 nb_file = 0
 nb_double = 0
@@ -30,6 +30,7 @@ url_resources = (
     "token="+token
 )
 ALL_MD5 = {}
+ALL_ID = {}
 try:
     resp = requests.get(url_resources, headers=headers)
     nb_request += 1
@@ -37,8 +38,11 @@ try:
     resp_dict = resp.json()
     #print(resp_dict)
     for my_resource in resp_dict:
-        print(my_resource.get('id'))
         my_id = my_resource.get('id')
+        my_title = my_resource.get('title')
+        my_mime = my_resource.get('mime')
+        my_ext = my_resource.get('file_extension')
+        my_type = my_resource.get('type_')
         url_one_resources = (
     "http://"+ip+":"+port+"/resources/"+my_resource.get('id')+"/file?"
     "token="+token
@@ -51,17 +55,21 @@ try:
                 for data in tqdm(resp2.iter_content()):
                       handle.write(data)
             my_md5 = hashlib.md5(open('myfile','rb').read()).hexdigest()
-            print("MD5:",my_md5)
-            if my_id in ALL_MD5:
+            #print("MD5:",my_md5)
+            if my_md5 in ALL_MD5:
                  print("Aie")
                  nb_double += 1
-                 ALL_MD5[my_id] = my_md5
+                 ALL_ID[my_id] = my_md5
+                 my_doublon = ALL_MD5[my_md5]
             else:
-                 ALL_MD5[my_id] = my_md5
+                 ALL_MD5[my_md5] = my_id
+                 ALL_ID[my_id] = my_md5
+                 my_doublon = ''
         except requests.exceptions.HTTPError as e:
             print("Bad HTTP status code:", e)
         except requests.exceptions.RequestException as e:
             print("Network error:", e)
+        print("LIST:",my_id,",",my_title,",",my_mime,",",my_ext,",",nb_file,",",my_type,",",nb_request,",MD5,",my_md5,",SAME_AS,",my_doublon,"\n")
 except requests.exceptions.HTTPError as e:
     print("Bad HTTP status code:", e)
 except requests.exceptions.RequestException as e:
